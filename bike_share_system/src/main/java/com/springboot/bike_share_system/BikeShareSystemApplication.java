786eae8116672a18c9846a7754a3b25dc4f04bf4
ds19970106 <ds1079436484@163.com>
2019-09-26 15:51:51 +0100

    Creat Project
    An empty Java project based on Spring boot

---
+++ bike_share_system/src/main/java/com/springboot/bike_share_system/BikeShareSystemApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.bike_share_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeShareSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeShareSystemApplication.class, args);
+    }
+
+}
---
+++ bike_share_system/src/test/java/com/springboot/bike_share_system/BikeShareSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot.bike_share_system;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BikeShareSystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
